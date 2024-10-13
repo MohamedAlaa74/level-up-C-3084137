fc0d374bc15165e98a819797373a429e4f9a645f
Mohamed Alaa <malaaeldeen74@gmail.com>
2024-10-13 09:23:13 +0300

    Update ordinal_challenge.c

--- src/01_01/ordinal_challenge.c
+++ src/01_01/ordinal_challenge.c
@@ -2,10 +2,22 @@
 
 char *ordinal(int v)
 {
-	/*
-	   - create this function
-	   - this code won't run with out it!
-	 */
+	if ((v % 10 == 11) || (v % 10 == 12) || (v % 10 == 13))
+	{
+		return "th";
+	}
+
+	switch (v % 10)
+	{
+	case 1:
+		return "st";
+	case 2:
+		return "nd";
+	case 3:
+		return "rd";
+	default:
+		return "th";
+	}
 }
 
 int main()
@@ -13,16 +25,15 @@ int main()
 	int c;
 
 	/* output a table of 100 numbers */
-	for( c=1; c<=20; c++ )
+	for (c = 1; c <= 20; c++)
 	{
 		printf("%3d%s\t%3d%s\t%3d%s\t%3d%s\t%3d%s\n",
-				c, ordinal(c),
-				c+20, ordinal(c+20),
-				c+40, ordinal(c+40),
-				c+60, ordinal(c+60),
-				c+80, ordinal(c+80)
-			  );
+					 c, ordinal(c),
+					 c + 20, ordinal(c + 20),
+					 c + 40, ordinal(c + 40),
+					 c + 60, ordinal(c + 60),
+					 c + 80, ordinal(c + 80));
 	}
 
-	return(0);
+	return (0);
 }
